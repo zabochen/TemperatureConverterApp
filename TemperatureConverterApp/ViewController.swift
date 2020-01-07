93f6c1128fc72dfc676623724f9eaebf3e1824f0
Taras <zabochen@gmail.com>
2020-01-07 10:55:45 +0200

    Edit "viewController"

--- TemperatureConverterApp/ViewController.swift
+++ TemperatureConverterApp/ViewController.swift
@@ -2,19 +2,30 @@
 //  ViewController.swift
 //  TemperatureConverterApp
 //
-//  Created by Taras on 05.01.2020.
-//  Copyright © 2020 Taras. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celsiumLabel: UILabel!
+    @IBOutlet weak var fahrenhaitLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.minimumValue = 0
+            slider.maximumValue = 100
+            slider.value = 0
+            slider.minimumTrackTintColor = .green
+            slider.maximumTrackTintColor = .red
+        }
     }
-
-
+    
+    @IBAction func sliderValueChange(_ sender: UISlider) {
+        let celsium = Int(round(sender.value))
+        celsiumLabel.text = "\(celsium)ºC"
+        
+        let fahrenhait = (sender.value * 9 / 5) + 32
+        fahrenhaitLabel.text = "\(Int(round(fahrenhait)))ºF"
+    }
 }
 
